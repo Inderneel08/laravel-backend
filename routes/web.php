6f9d5f096475788d9b332d006e67066604d3e18b
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-09 10:41:01 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,16 @@ use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/api/login',[ApiController::class,'login'])->name('login');
+Route::post('/api/auth/login',[ApiController::class,'login'])->name('login');
 
-Route::post('/api/register',[ApiController::class,'register'])->name('register');
+Route::post('/api/auth/register',[ApiController::class,'register'])->name('register');
+
+
 
 Route::middleware('custom.token')->group(function(){
-    // Route::get('/user',function(){
-
-    // });
+    Route::get('/api/auth/check',function(){
+        return response()->json(['message' => 'You are authenticated']);
+    });
 
     Route::post('/api/logout',[ApiController::class, 'logout'])->name('logout');
 });
