1f5a38b1d1d299d882ab2c0a152e878cc276ec5c
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-09 11:34:01 +0530

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['category_name'];
+
+    protected $table='category';
+
+    public $timestamps = false;
+
+    public function products()
+    {
+        $this->belongsTo(Products::class);
+    }
 }
