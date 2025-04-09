302223f3b047c96404e99c3292e221c5c7df721a
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-09 13:02:42 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,7 +9,9 @@ Route::post('/api/auth/login',[ApiController::class,'login'])->name('login');
 
 Route::post('/api/auth/register',[ApiController::class,'register'])->name('register');
 
+Route::get('/api/products',[ProductController::class, 'getproducts'])->name('getProducts');
 
+Route::get('/api/products/{id}',[ProductController::class,'getProductInfo'])->name('getProductInfo');
 
 Route::middleware('custom.token')->group(function(){
     Route::get('/api/auth/check',function(){
