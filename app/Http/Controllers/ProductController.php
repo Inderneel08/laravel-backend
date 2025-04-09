f94b7464a64b7d374f9d3ab0780f0e461934cfdb
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-09 12:30:44 +0530

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+
+    public function getproducts()
+    {
+        $products = Products::all();
+
+        foreach ($products as $product) {
+            $product->image = asset('public/'. $product->image);
+        }
+
+        return(response()->json(['products'=>$products]));
+    }
 }
