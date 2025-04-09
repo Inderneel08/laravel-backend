4792aba94a840b874cb1f275ed8345c3511288bd
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-09 11:33:44 +0530

    Update Products.php

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+    protected $fillable = ['title', 'price','description','category','image','rate_id'];
+
+    protected $table='products';
+
+    public $timestamps = false;
+
+    public function ratings()
+    {
+        return($this->hasMany(Rating::class,'rate_id'));
+    }
+
+    public function categories()
+    {
+        return($this->hasMany(Category::class,'category'));
+    }
 }
