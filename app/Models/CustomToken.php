bee29144677568b29373aa4cdf5cbe7d1a0f2ed0
Inderneel08 <inderneel.minhas@gmail.com>
2026-05-17 12:40:34 +0530

    New changes.

--- app/Models/CustomToken.php
+++ app/Models/CustomToken.php
@@ -3,16 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CustomToken extends Model
 {
-    //
+    use SoftDeletes;
 
     protected $fillable = ['user_id', 'token_value'];
 
     protected $table='token_table';
 
     public $timestamps = false;
+
+    protected $dates = [
+        'created_at',
+        'deleted_at',
+    ];
 
     public function user()
     {
